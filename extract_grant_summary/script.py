2ce40e4f945fcd6ac661330dbe4a248426e3313a
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2019-11-14 15:56:59 -0500

    update extract_grant_summary script to pull from ID

--- extract_grant_summary/script.py
+++ extract_grant_summary/script.py
@@ -2,13 +2,13 @@ import os
 import glob
 import json
 from bs4 import BeautifulSoup
-from twilio.rest import Client
+#  from twilio.rest import Client
 
-account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
-auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
-twilio_client = Client(account_sid, auth_token)
+#  account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+#  auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+#  twilio_client = Client(account_sid, auth_token)
 
-samples = glob.glob("../pull_paginated_pages_of_awards/html/*.html")
+samples = glob.glob("./pull_paginated_pages_of_awards/sample/*.html")
 
 for i, sample in enumerate(samples):
     with open(sample) as f:
@@ -20,14 +20,14 @@ for i, sample in enumerate(samples):
             grants.append({
                 "title": title.get_text(),
                 "detail_url": f"http://www.sbir.gov{title['href']}",
-                "sbir_id": title['href'][-7:]
+                "sbir_id": title['href'].split('/')[-1]
             })
-            
-        with open(f"./grants/{i}.json", 'w+') as jsonFile:
+
+        with open(f"./extract_grant_summary/samples/{i}.json", 'w+') as jsonFile:
             json.dump(grants, jsonFile, indent=2)
 
-message = twilio_client.messages.create(
-    body="finished extracting grant summaries as json",
-    from_="+17027663746",
-    to="+17132809889"
-)
+#  message = twilio_client.messages.create(
+#      body="finished extracting grant summaries as json",
+#      from_="+17027663746",
+#      to="+17132809889"
+#  )
